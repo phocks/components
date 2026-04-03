baf6bf7040db067e5f40ae76a6ac79c182727e0e
Joshua Byrd <phocks@gmail.com>
2026-04-03 17:25:58 +1000

    Hello!

--- main.tsx
+++ main.tsx
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import type { FC } from "hono/jsx";
 import { poweredBy } from "hono/powered-by";
 import { logger } from "hono/logger";
 
@@ -21,15 +22,37 @@ app.get("/page", (c) => {
   return c.html(<View />);
 });
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
-
 app.get("/api/hello", (c) => {
   return c.json({
     ok: true,
     message: "Hello Hono!",
   });
+});
+
+const Layout: FC = (props) => {
+  return (
+    <html>
+      <body>{props.children}</body>
+    </html>
+  );
+};
+
+const Top: FC<{ messages: string[] }> = (props: { messages: string[] }) => {
+  return (
+    <Layout>
+      <h1>Hello Hono!</h1>
+      <ul>
+        {props.messages.map((message) => {
+          return <li>{message}!!</li>;
+        })}
+      </ul>
+    </Layout>
+  );
+};
+
+app.get("/", (c) => {
+  const messages = ["Good Morning", "Good Evening", "Good Night"];
+  return c.html(<Top messages={messages} />);
 });
 
 Deno.serve(app.fetch);
